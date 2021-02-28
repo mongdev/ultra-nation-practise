181e5c02f7bb62facfe9ecb14307d59a9fbdb255
Mongyu Marma <mongyu.html@gmail.com>
2021-02-28 19:33:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Cart from "./components/cart/Cart";
+import Country from "./components/country/Country";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Country Loaded: {countries.length}</h1>
+      <h3>Country Added: {cart.length}</h3>
+      <Cart cart={cart}></Cart>
+
+      {countries.map((country) => (
+        <Country
+          country={country}
+          handleAddCountry={handleAddCountry}
+          key={country.alpha3Code}
+        ></Country>
+      ))}
+
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/components/country/Country.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Country = (props) => {
+  const { name, population, region, flag } = props.country;
+  const flagStyle = { height: "50px" };
+  const countryStyle = {
+    border: "1px solid red",
+    margin: "10px",
+    padding: "10px",
+  };
+  const handleAddCountry = props.handleAddCountry;
+
+  return (
+    <div style={countryStyle}>
+      <h3>This is a {name}</h3>
+      <img style={flagStyle} src={flag} alt="" />
+      <p>Population :{population} </p>
+      <p>Region :{region}</p>
+      <button onClick={() => handleAddCountry(props.country)}>
+        Add Country
+      </button>
+    </div>
+  );
+};
+
+export default Country;
